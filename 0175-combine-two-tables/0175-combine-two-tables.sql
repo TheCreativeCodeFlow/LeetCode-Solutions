63b8c993e66d8dcd962f550b3cd6e47d490b5522
Rahul Seervi <thecreativecodeflow@gmail.com>
2026-07-26 00:22:24 +0530

    Time: 557 ms (10.11%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,8 +1,1 @@
-SELECT
-    Person.firstName,
-    Person.lastName,
-    Address.city,
-    Address.state
-FROM Person
-LEFT JOIN Address
-    ON Person.personID=Address.personID;
+SELECT Person.firstName, Person.lastName, Address.city, Address.state From Person left join Address on Person.personID=Address.personID
